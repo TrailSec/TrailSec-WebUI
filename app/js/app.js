bc0cbb398678b5ead244a98ddc1aeb0d70451fc8
Justin Toh <tohjustin@hotmail.com>
2017-02-07 14:45:39 -0800

    Added Vue.js library to power sidepanel

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
+import Vue from './libs/vue.js'
 import GoogleMapsHelper from './google-maps-helper'
 import Routes from './route-data'
 
@@ -9,6 +10,24 @@ var GOOGLE_MAPS = {
   infoWindows: [],
   routes: []
 }
+
+// eslint-disable-next-line no-unused-vars
+var vm = new Vue({
+  el: '#sidepanel',
+  data: {
+    coordinates: [
+      {content: 'Justin Toh has arrived at checkpoint #A-13', time: '1st Jan 2017, 12:40:12 (12mins ago)'},
+      {content: 'Justin Toh has arrived at checkpoint #A-12', time: '1st Jan 2017, 12:39:59 (13mins ago)'},
+      {content: 'Justin Toh has arrived at checkpoint #A-11', time: '1st Jan 2017, 12:38:05 (14mins ago)'},
+      {content: 'Justin Toh has arrived at checkpoint #A-10', time: '1st Jan 2017, 12:37:10 (15mins ago)'}
+      // {content: 'Justin Toh has arrived at checkpoint #A-09', time: '1st Jan 2017, 12:38:10 (16mins ago)'},
+      // {content: 'Justin Toh has arrived at checkpoint #A-08', time: '1st Jan 2017, 12:39:10 (17mins ago)'},
+      // {content: 'Justin Toh has arrived at checkpoint #A-07', time: '1st Jan 2017, 12:39:10 (17mins ago)'},
+      // {content: 'Justin Toh has arrived at checkpoint #A-06', time: '1st Jan 2017, 12:40:10 (18mins ago)'},
+      // {content: 'Justin Toh has arrived at checkpoint #A-05', time: '1st Jan 2017, 12:41:10 (19mins ago)'}
+    ]
+  }
+})
 
 GoogleMapsHelper.createMap(GOOGLE_MAPS)
 
