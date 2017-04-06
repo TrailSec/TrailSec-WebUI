9b6053e9c32661eb0558407b96dd59163714a166
Justin Toh <tohjustin@hotmail.com>
2017-04-05 23:01:40 -0700

    Switched firebase account

--- app/js/app.js
+++ app/js/app.js
@@ -122,7 +122,7 @@ document.querySelector('#toDatetimeBtn')
  *  - Listen for database updates in realtime
  *  - On every update, feed date into our sidepanel via Vue.js
  ****************************************************************/
-firebase.initializeApp({databaseURL: 'https://cpen391-trailsec.firebaseio.com/'})
+firebase.initializeApp({databaseURL: 'https://cpen391-poc.firebaseio.com/'})
 firebase.database().ref('/Geolocation').on('value', snapshot => {
   vm.checkInData = _.chain(snapshot.val())
                       .toArray()                      // convert `rawData` into an array (removes firebase's pushId keys)
